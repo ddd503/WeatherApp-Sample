6b2748c3f17df8c8d4b523094804ac725ae4dafa
Dai Kawahara <skbsporta7@gmail.com>
2019-09-06 22:28:53 +0900

    sepalate directory by responsibility

---
+++ WeatherApp-Sample/View/WeatherViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp-Sample
+//
+//  Created by kawaharadai on 2019/09/06.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    init() {
+        super.init(nibName: String(describing: WeatherViewController.self), bundle: .main)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
