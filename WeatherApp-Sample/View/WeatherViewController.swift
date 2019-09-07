885f826d1798eec3eeaf02c400e95714cdadd2ef
Dai Kawahara <skbsporta7@gmail.com>
2019-09-07 17:16:54 +0900

    add subviews in WeatherVC

--- WeatherApp-Sample/View/WeatherViewController.swift
+++ WeatherApp-Sample/View/WeatherViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+final class WeatherViewController: UIViewController {
+
+    @IBOutlet private weak var forecastView: UIView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var weatherImageView: UIImageView!
+    @IBOutlet private weak var weatherTitleLabel: UILabel!
+    @IBOutlet private weak var minTemperatureLabel: UILabel!
+    @IBOutlet private weak var maxTemperatureLabel: UILabel!
+    @IBOutlet private weak var summaryLabel: UILabel!
+    @IBOutlet private weak var otherDaysForecastsView: UITableView! {
+        didSet {
+            otherDaysForecastsView.dataSource = self
+            otherDaysForecastsView.delegate = self
+        }
+    }
 
     let presenter: WeatherViewPresenterInputs!
 
@@ -32,3 +46,15 @@ class WeatherViewController: UIViewController {
 extension WeatherViewController: WeatherViewPresenterOutputs {
 
 }
+
+extension WeatherViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension WeatherViewController: UITableViewDelegate {}
